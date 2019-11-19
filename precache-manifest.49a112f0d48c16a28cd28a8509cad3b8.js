e2a1b70f0fb4cdfc1849bd84f8c4a9e6c32906a2
sahilarora1511 <sahilarora1511@github.com>
2019-11-19 15:50:01 +0530

    Updates

---
+++ precache-manifest.49a112f0d48c16a28cd28a8509cad3b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17cb882ab92930b84af8118cccb965b",
+    "url": "/App/index.html"
+  },
+  {
+    "revision": "40436c1cafc97dd4c384",
+    "url": "/App/static/css/main.0a1103a6.chunk.css"
+  },
+  {
+    "revision": "d702ba62303b88bbe1dc",
+    "url": "/App/static/js/2.f80a1cbe.chunk.js"
+  },
+  {
+    "revision": "40436c1cafc97dd4c384",
+    "url": "/App/static/js/main.1643f40a.chunk.js"
+  },
+  {
+    "revision": "d8cc58f0e3f8fcde8360",
+    "url": "/App/static/js/runtime-main.c5bd9b72.js"
+  }
+]);
